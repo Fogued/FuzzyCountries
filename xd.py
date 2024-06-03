e85755f4e1c8b2a70bd7cd7539b84279f823cf66
Javier Comyn <64434554+JComyn@users.noreply.github.com>
2024-06-03 13:36:53 +0200

    Delete xd.py

--- xd.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Print column Population without the index
-df = pd.read_csv('data-pancho/cleaned-all.csv')
-column = df['Minimum wage']
-# Quitar el .0 decimal
-column = column.astype(int)
-print(column.to_string(index=False))
